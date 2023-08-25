890f8e387a070d8ce8b50a84c5850fba0ce53f19
João Paulo Falcão <94097333+jplfalcao@users.noreply.github.com>
2023-08-25 11:16:11 -0300

    Python File

--- 1-if.py
+++ 1-if.py
@@ -1,35 +1,75 @@
-#!/usr/bin/env python
-# Autor: João Falcão
+# Autor: João PauLo Falcão
 # Github: https://github.com/jplfalcao
 # Data de criação: 28/10/2022
-# Data de modificação:
-# Versão: 1.0
-# Uso: python3 1-if.py
+# Data de modificação: 25/08/2023
+# Versão: 1.1
 
-# ESTRUTURAS CONDICIONAIS: if elif else
 
-# A estrutura do código segue a PEP8.
-# https://peps.python.org/pep-0008/
+"""
+Dicas:
+Utilize quatro espaços em branco para indentação!
+Termine uma instrução sempre com uma nova linha!
 
-# Documentação consultada:
-# https://docs.python.org/3/tutorial/controlflow.html#if-statements
+Referências:
+https://peps.python.org/pep-0008/
+https://wiki.python.org.br/GuiaDeEstilo
+"""
 
-# O padrão de nomeamento de variáveis que o python segue é chamado
-# snakecase: nome_da_variavel = 123.
 
+# ESTRUTURA CONDICIONAL
+
+"""
+O if (Se), elif (else if | Senão Se), else (Senão), nos permite executar ações
+alternativas com base em condições (testes).
+"""
+
+print("\n>>>Média Escolar<<<")
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 nota3 = float(input("Digite a terceira nota: "))
 nota4 = float(input("Digite a quarta nota: "))
-media = (nota1 + nota2 + nota3 + nota4) / 4
-print("\nSua média foi:", media)
 
-# Estrutura que será testada.
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(f"\nMédia: {media:.2f}")
+
+# Condição que será testada com o comando 'if'
+# Se for verdadeiro, o bloco de código é executado
 if media >= 7:
-	print("Resultado: Aprovado! :-)")
-# Quando o teste do if é negado, é efetuado outro teste com o elif.
-elif (media >= 6) and (media <= 6.9):
-	print("Resultado: Prova final! :-0")
-# Quando nenhuma das condições são satisfeitas, é executado o else.
+    print("Resultado: Aprovado! :-)\n")
+# Quando o teste do 'if' é falso, é efetuado outro teste com o comando 'elif'
+elif media >= 6 and media < 7:
+    print("Resultado: Prova final! :-/\n")
+# Quando nenhuma das condições é verdadeira, é executado o comando 'else'
 else:
-	print("Resultado: Reprovado! :-(")
+    print("Resultado: Reprovado! :-(\n")
+
+
+
+print("\n>>>Índice de Massa Corporal<<<")
+peso = float(input("Informe seu peso: "))
+altura = float(input("Informe sua altura: "))
+
+imc = peso / (altura ** 2)
+
+print(f"\nIMC: {imc:.2f}")
+
+if imc < 18.50:
+    print("Abaixo do peso!\n")
+elif imc >= 18.50 and imc <= 24.99:
+    print("Peso normal!\n")
+elif imc >= 25.00 and imc <= 29.99:
+    print("Acima do peso!\n")
+else:
+    print("Obesidade!\n")
+
+
+
+"""
+Referências:
+https://docs.python.org/3/tutorial/controlflow.html#if-statements
+https://docs.python.org/3/reference/compound_stmts.html#the-if-statement
+https://www.w3schools.com/python/python_conditions.asp
+https://www.geeksforgeeks.org/python-if-else/
+https://www.minhavida.com.br/saude/tratamento/3870-imc
+"""
