4bf85555cf51a1b86a720cfb4a990400401fce82
João Paulo Falcão <94097333+jplfalcao@users.noreply.github.com>
2023-08-25 13:31:21 -0300

    Python File

--- 2-while.py
+++ 2-while.py
@@ -1,38 +1,66 @@
-#!/usr/bin/env python
-# Autor: João Falcão
+# Autor: João PauLo Falcão
 # Github: https://github.com/jplfalcao
 # Data de criação: 28/10/2022
-# Data de modificação:
-# Versão: 1.0
-# Uso: python3 2-while.py
+# Data de modificação: 25/08/2023
+# Versão: 1.1
+
+
+"""
+Dicas:
+Utilize quatro espaços em branco para indentação!
+Termine uma instrução sempre com uma nova linha!
+
+Referências:
+https://peps.python.org/pep-0008/
+https://wiki.python.org.br/GuiaDeEstilo
+"""
+
 
 # ESTRUTURA DE REPETIÇÃO: while
 
-# A estrutura do código segue a PEP8.
-# https://peps.python.org/pep-0008/
+"""
+O loop 'while' será repetido enquanto a expressão booleana for verdadeira.
+Expressão booleane é toda expressão onde o resultado será verdadeiro (True)
+ou falso (False).
 
-# Documentação consultada:
-# https://docs.python.org/3/reference/compound_stmts.html#while
+IMPORTANTE: O LOOP 'WHILE' PRECISA DE UM CRITÉRIO DE PARADA, PARA NÃO SE TORNAR
+UM LOOP INFINITO.
 
-# O padrão de nomeamento de variáveis que o python segue é chamado
-# snakecase: nome_da_variavel = 123.
+O 'while' testa repetidamente a expressão e, se for verdadeira, 
+executa o primeiro conjunto; se a expressão for falsa 
+(que pode ser a primeira vez que é testada), o conjunto da cláusula 'else', 
+se presente, é executado e o loop é encerrado.
+"""
 
-import time  # Importando o módulo "time".
+import time  # Importando o módulo 'time'
 
 inicio = 0
 fim = 10
-while inicio <= fim:  # Definimos o número de repetições
-    # (O contador chegará até o valor da variável "fim").
-    inicio += 1  # Variável contadora que será incrementada até chegar
-    # ao número que foi definido.
-    print("Número:", inicio)
-    time.sleep(1)  # "Sleep" é um método do módulo "time".
+while inicio < fim:
+    # Contador que será incrementado até chegar ao valor da variável 'fim'
+    inicio += 1
+    print(f"Número: {inicio}")
+    # O 'sleep' é um método do módulo 'time', onde foi definido o atraso de 1s
+    time.sleep(1)
     if inicio == 5:
         print("Paramos a contagem por aqui!\n")
-        break  # O comando "break" sai imediatamente do laço de repetição.
+        # A instrução 'break' sai imediatamente do laço de repetição
+        break
 
+
+
+print("\n>>>Login no Sistema<<<")
 senha = input("Digite sua senha: ")
 
 while senha != "abc123":
     senha = input("Senha inválida! Tente novamente: ")
 print("\nAcesso permitido!")
+
+
+
+"""
+Referências:
+https://docs.python.org/3/reference/compound_stmts.html#while
+https://docs.python.org/3/library/time.html?highlight=time#time.sleep
+https://www.w3schools.com/python/python_while_loops.asp
+"""
